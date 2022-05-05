a27d3fb14756e82bea33f514dbeec897aed64089
keithyyeo <s10206050@connect.np.edu.sg>
2022-05-05 16:00:01 +0800

    updated the Challenges component

--- web-app/src/components/Challenges/Challenges.js
+++ web-app/src/components/Challenges/Challenges.js
@@ -5,6 +5,8 @@ import Card from "react-bootstrap/Card";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom";
 import { ChallengesData } from './ChallengesData';
+import { BsStarFill, BsStar } from 'react-icons/bs';
+import { Row, Col } from 'react-bootstrap';
 
 
 class Challenges extends React.Component{
@@ -13,6 +15,21 @@ class Challenges extends React.Component{
     }
 
     render() {
+        function rating(rate) {
+            const items = []
+
+            for (var i = 0; i < rate; i++) {
+                items.push(<BsStarFill />)
+            }
+            if (rate < 5 ) {
+                var i = 5 - rate
+                for (var j = 0; j < i; j++) {
+                    items.push(<BsStar />)
+                }
+            }
+            return items
+        }
+
         return (
             <Card className={styles.wrapper}>     
                 <Card.Title>Challenges</Card.Title>       
@@ -24,11 +41,25 @@ class Challenges extends React.Component{
                         //     {item.desc}
                         // </p>
                         <Link key={`${index}`} to={`/challenges/${item.to}`} className={styles.announcement}>
-                            <p>
-                                {item.challenge}
-                                <br />
-                                By: {item.by}
-                            </p>
+                            <Row>
+                                <Col>
+                                    <p>
+                                        {item.challenge}
+                                        <br />
+                                        By: {item.by}
+                                        <br />
+                                        Rating: {rating(item.rating)}
+                                    </p>
+                                </Col>
+                                <Col>
+                                    <h2 className={styles.right}>
+                                        {item.points} 
+                                        <br />
+                                        points
+                                    </h2>
+                                </Col>
+                            </Row>
+
                         </Link>
                     )
                 }        
--- web-app/src/components/Challenges/ChallengesData.js
+++ web-app/src/components/Challenges/ChallengesData.js
@@ -5,12 +5,16 @@ export const ChallengesData = [
         challenge: 'Challenge 1', 
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         to: 'challenge1',
-        by: 'Adam'
+        by: 'Adam',
+        rating: 5,
+        points: 100,
     },
     {
         challenge: 'Challenge 2', 
         desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         to: 'challenge2',
-        by: 'Adam'
+        by: 'Adam',
+        rating: 3,
+        points: 10,
     },
 ];
