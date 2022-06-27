550703f8af753eda410cb2fc2a028390036f6a6b
reeeeneeeee <s10206117@connect.np.edu.sg>
2022-06-28 01:28:20 +0800

    harry the hamster 2.0

--- web-app/src/pages/Game/Game.js
+++ web-app/src/pages/Game/Game.js
@@ -10,59 +10,76 @@ import Image from 'react-bootstrap/Image';
 const Game = props=>{
     const [hunger , setHunger ] = useState(1);
     const [cleanliness , setCleanliness ] = useState(1);
-    const [health , setHealth ] = useState(1);
     const [happiness , setHappiness ] = useState(1);
     const [level , setLevel ] = useState(1);
 
+    const checkHappy = () => {
+        if (happiness < 10 ) {
+            setHappiness(happiness + 1)
+        } else {
+            alert("Harry likes being pet, but not THIS much")
+        }
+    }
+
+    const checkHunger = () => {
+        if (hunger < 10 ) {
+            setHunger(hunger + 1)
+        } else {
+            alert("Harry feels like he is bursting at the seams")
+        }
+    }
+
+    const checkClean = () => {
+        if (cleanliness < 10 ) {
+            setCleanliness(cleanliness + 1)
+        } else {
+            alert("Harry feels SQUEAKY clean!")
+        }
+    }
+
     useEffect(() => {
-        if (hunger == 10 && cleanliness == 10 && health == 10 && happiness == 10) {
+        if (hunger == 10 && cleanliness == 10 && happiness == 10) {
             setLevel(level + 1);
             setHunger(0);
             setCleanliness(0);
-            setHealth(0);
             setHappiness(0);
         }
-        console.log(hunger, cleanliness, health, happiness, level);
-    }, [hunger, cleanliness, health, happiness]);
+        console.log(hunger, cleanliness,  happiness, level);
+    }, [hunger, cleanliness, happiness]);
+    
     
 
     return (
         <PageContainer>
-            <h1>Game</h1>
-            <div>
-                <p>Level = {level}</p>
-                <p>Hunger = {hunger} </p>
-                <p>Cleanliness = {cleanliness} </p>
-                <p>Health = {health} </p>
-                <p>Happiness = {happiness} </p>
-            </div>
-            <div>
-                <Image src='media/game/vpet.png'></Image>
-            </div>
-            <div>
-                <Button variant='primary' onClick = {() => {
-                    if (hunger < 10 ) {
-                        setHunger(hunger + 1)
-                    }
-                }}>Feed Pet</Button>
+            <div className={styles.contents} class="container">
+                <div class="row align-items-start">
+                    <div className={styles.stats} class="col align-self-center">
+                        <h1>Harry the Hamster</h1>
+                        <h2>Level = {level}</h2>
+                        <p>Happiness = {happiness}</p>
+                        <p>Food Received = {hunger}</p>
+                        <p>Cleanliness = {cleanliness}</p>
+                    </div>
+                </div>
 
-                <Button variant='danger' onClick = {() => {
-                    if (cleanliness < 10 ) {
-                        setCleanliness(cleanliness + 1)
-                    }
-                }}>Scrub Pet</Button>
+                <div class="row align-items-center">
+                    <div class="col">
+                        <Image className={styles.icon} src='media/game/soap.png' onClick={checkClean}></Image>
+                        <label style={{color: 'palevioletred'}}>Clean Me!</label>
+                    </div>
+                    <div class="col">
+                    <label style={{color: 'green'}}>Feed Me!</label>
+                    <Image className={styles.icon} src='media/game/food.png' onClick={checkHunger}></Image>
+                    </div>
+                </div>
+        
+                <div class="row align-items-end">
+                    <div class="col align-self-center">
+                        <Image src='media/game/vpet.png' onMouseOver={checkHappy}></Image>
+                        <p className={styles.petLabel}>Pet Me to Make Me Happy!</p>
+                    </div>  
+                </div>
 
-                <Button variant='success'onClick = {() => {
-                    if (health < 10 ) {
-                        setHealth(health + 1)
-                    }
-                }}>Heal Pet</Button>
-
-                <Button variant='warning'onClick = {() => {
-                    if (happiness < 10 ) {
-                        setHappiness(happiness + 1)
-                    }
-                }}>Stroke Pet</Button>
             </div>
             
         </PageContainer>
